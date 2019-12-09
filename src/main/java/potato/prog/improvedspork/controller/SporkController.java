fd5745310040e1056906559432e8c942666a7481
potato-prog <53585256+potato-prog@users.noreply.github.com>
2019-12-08 22:16:46 -0600

    add swagger...

--- src/main/java/potato/prog/improvedspork/controller/SporkController.java
+++ src/main/java/potato/prog/improvedspork/controller/SporkController.java
@@ -2,10 +2,12 @@ package potato.prog.improvedspork.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SporkController {
 
+    @ResponseBody
     @GetMapping("/spork")
     public String displayWelcomeMessage() {
         return "Improved Spork";
